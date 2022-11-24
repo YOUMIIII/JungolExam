fc2f0bd7c526939b0c8d5e338efefcf4a066e0cd
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 10:08:59 +0900

    정올 536번

---
+++ JungolTest/06Repeat/q536/Main.java
@@ -0,0 +1,32 @@
+/*
+1부터 15까지 차례로 출력하는 프로그램을 작성하시오.
+*/
+
+package q536;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// 1. for loop
+		for (int i = 1; i <= 15; i++) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println();
+		
+		// 2. while loop
+//		int j = 1;
+//
+//		while (j <= 15) {
+//			System.out.print(j++ + " ");
+//		}
+//		
+//		System.out.println();
+//		
+		// 3. do-while loop
+//		int k = 1;
+//		do {
+//			System.out.print(k + " ");
+//		} while (k++ < 15);
+	}
+}
