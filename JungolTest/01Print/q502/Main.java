eb7c2fb0c8cd51c44b078377f347f7d771fc49d9
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 10:12:24 +0900

    정올 502번

---
+++ JungolTest/01Print/q502/Main.java
@@ -0,0 +1,14 @@
+/*
+다음과 같이 출력되는 프로그램을 작성하라.
+<출력 예>
+Programming! It's fun.
+*/
+
+package q502;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("My name is Hong Gil Dong.");
+		System.out.println("I am 13 years old.");
+	}
+}
