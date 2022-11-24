ae23ff782efbf1f4d73f42ff122007005e3aa1b4
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 09:59:21 +0900

    정올 528번

--- JungolTest/05Select/q528/Main.java
+++ JungolTest/05Select/q528/Main.java
@@ -1,3 +1,12 @@
+/*
+정수를 입력받아 첫 줄에 입력 받은 숫자를 출력하고 둘째 줄에 음수이면 “minus” 라고 출력하는 프로그램을 작성하시오.
+<입력 예>
+-5
+<출력 예>
+-5
+minus
+*/
+
 package q528;
 
 import java.util.Scanner;
@@ -7,10 +16,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		sc.close();
-		
+
 		System.out.println(num);
-		
-		if (num<0) {
+
+		if (num < 0) {
 			System.out.println("minus");
 		}
 	}
