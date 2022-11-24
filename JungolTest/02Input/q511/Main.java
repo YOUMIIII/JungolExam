569b3de0e3ec4f44153ef91e93588c44113f5b6c
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 09:46:19 +0900

    정올 511번

---
+++ JungolTest/02Input/q511/Main.java
@@ -0,0 +1,22 @@
+/*
+두 개의 정수형 변수를 선언하고 값을 대입하여 아래와 같이 출력되는 프로그램을 작성하라.
+<출력 예>
+55 - 10 = 45
+2008 - 1999 = 9
+*/
+
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 55;
+		int y = 10;
+		System.out.println(x + " - " + y + " = " + (x-y));
+		
+		x = 2008;
+		y = 1999;
+		System.out.println(x + " - " + y + " = " + (x-y));
+		
+	}
+}
