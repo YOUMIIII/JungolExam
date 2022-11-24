7ec657aa260ba0496dfd5a6d215f7dced4514bdc
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 10:14:01 +0900

    정올 509번

---
+++ JungolTest/02Input/q509/Main.java
@@ -0,0 +1,16 @@
+/*
+정수형 변수를 선언하고 -100을 대입하여 출력하는 프로그램을 작성하라.
+*/
+
+package q509;
+
+public class Main {
+	public static void main(String[] args) {
+		int num = -100;
+
+//		int num;
+//		num = -100; 이렇게 풀어도 됨.
+
+		System.out.println(num);
+	}
+}
