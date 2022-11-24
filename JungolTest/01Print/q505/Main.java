03520826f36ed7b622b6ff8e270d5810eafedb17
Administrator <Administrator@User-2022ZZXAVA>
2022-11-24 09:42:40 +0900

    정올 505번

---
+++ JungolTest/01Print/q505/Main.java
@@ -0,0 +1,18 @@
+/*
+서식 문자를 사용하여 다음과 같이 출력되는 프로그램을 작성하라.
+<출력 예>
+I can program well.
+Dreams come true.
+*/
+
+package q505;
+
+public class Main {
+	public static void main(String[] args) {
+		String str1 = "I can program well.";
+		String str2 = "Dreams come true.";
+		
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+}
