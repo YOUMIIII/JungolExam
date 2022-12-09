6df92d6ea0138e8d96f8eb27b385633ad5c8fec3
YOUMIIII <youmi8255@naver.com>
2022-12-09 17:36:48 +0900

    정올 9039번

---
+++ JungolTest/06Repeat/q9039/Main.java
@@ -0,0 +1,13 @@
+/*
+알파벳 'A'부터 'Z'까지 출력하는 프로그램을 작성하시오.
+*/
+
+package q9039;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i = 65; i<=90; i++) {
+			System.out.print((char)i);
+		}
+	}
+}
