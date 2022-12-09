903937bf8d2610317c99aab6edc997681eb904d6
YOUMIIII <youmi8255@naver.com>
2022-12-09 17:33:47 +0900

    정올 9047번

---
+++ JungolTest/07Repeat(2)/q9047/Main.java
@@ -0,0 +1,13 @@
+/*
+대문자를 'A'부터 'Z'까지 차례로 출력하는 프로그램을 작성하시오. 
+*/
+
+package q9047;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i = 65; i <= 90; i++) {
+			System.out.print((char)i);
+		}
+	}
+}
