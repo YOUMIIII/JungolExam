ca15c126efccc7749828beaf152c70e2a6582b72
YOUMIIII <youmi8255@naver.com>
2022-12-09 17:37:38 +0900

    정올 9041번

---
+++ JungolTest/06Repeat/q9041/Main.java
@@ -0,0 +1,25 @@
+/*
+점수를 입력받아 80점 이상이면 합격메시지를 그렇지 않으면 불합격 메시지를 출력하는 작업을 반복하다가 0~100점 이외의 점수가 입력되면 종료하는 프로그램을 작성하시오.
+*/
+
+package q9041;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.print("점수를 입력하세요. ");
+			int score = sc.nextInt();
+			if (score < 0 || score >= 100) {
+				sc.close();
+				break;
+			} else if (score >= 80) {
+				System.out.println("축하합니다. 합격입니다.");
+			} else {
+				System.out.println("죄송합니다. 불합격입니다.");
+			}
+		}
+	}
+}
