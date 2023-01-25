a5eb6467fd33cf427041a6a74a4d4f99b71f91fd
YOUMIIII <youmi8255@naver.com>
2023-01-25 15:03:35 +0900

    정올 9052번

---
+++ JungolTest/07Repeat(2)/q9052/Main.java
@@ -0,0 +1,14 @@
+//5명의 성적을 입력받아서 총점과 평균을 출력하는 프로그램을 작성하시오. (평균은 반올림하여 소수 첫째자리까지 출력한다.)
+
+package q9052;
+
+import java.util.Scanner;
+
+public class Main {
+    int[] score = new int[5];
+    Scanner sc = new Scanner(System.in);
+    for(int i = 0; i < score.length; i++){
+        score[i] = sc.nextInt();
+    }
+
+}
