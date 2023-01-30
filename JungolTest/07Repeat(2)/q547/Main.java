5d49b11cf234b95f2f3a3ade8400addb547cd3bf
YOUMIIII <youmi8255@naver.com>
2023-01-30 17:45:35 +0900

    정올 547번

---
+++ JungolTest/07Repeat(2)/q547/Main.java
@@ -0,0 +1,12 @@
+package q547;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 2; j <= 6; j++) {
+                System.out.print(j + i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
