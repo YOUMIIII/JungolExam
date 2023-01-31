891b93582075ef9f414f51160817aef9e22fc0e7
YOUMIIII <youmi8255@naver.com>
2023-01-31 17:45:10 +0900

    정올 549번

---
+++ JungolTest/08Repeat(3)/q549/Main.java
@@ -0,0 +1,23 @@
+package q549;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int sum = 0, count = 0;
+        for(int i = 1; i<=n; i++){
+            if(i%2 == 1){
+                sum += i;
+                count++;
+            }
+            if(sum > n){
+                break;
+            }
+        }
+        System.out.println(count + " " + sum);
+    }
+}
